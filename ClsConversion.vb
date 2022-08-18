cf70c5257eab87316345c99487ad35aa9442292d
bohemian9485 <tubianosaric26@gmail.com>
2022-08-18 12:16:03 +0800

    Add project files.

---
+++ ClsConversion.vb
@@ -0,0 +1,84 @@
+﻿Public Class ClsConversion
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return MyBase.ToString()
+    End Function
+
+    Public Overrides Function Equals(obj As Object) As Boolean
+        Return MyBase.Equals(obj)
+    End Function
+
+    Public Overrides Function GetHashCode() As Integer
+        Return MyBase.GetHashCode()
+    End Function
+
+    Private workMinutes As Integer
+    Public WriteOnly Property NewWorkMinutes() As Integer
+        Set(value As Integer)
+            workMinutes = value
+        End Set
+    End Property
+
+    Private carryCriteria As Integer
+    Public WriteOnly Property NewCarryOverCriteria() As Integer
+        Set(value As Integer)
+            carryCriteria = value
+        End Set
+    End Property
+
+    Public Sub GetClassSettings()
+        With My.Settings
+            workMinutes = .TotalWorkMinutes
+            carryCriteria = .CarryOverCriteria
+        End With
+    End Sub
+
+    Public Sub SaveClassSettings()
+        With My.Settings
+            .TotalWorkMinutes = workMinutes
+            .CarryOverCriteria = carryCriteria
+            .Save()
+        End With
+    End Sub
+
+    Public Function FractionsOfADay(ByVal Minutes As Integer) As Decimal
+        FractionsOfADay = 0
+        Try
+            If workMinutes > 0 AndAlso carryCriteria > 0 Then
+                ' Changes Minutes into decimal number
+                Dim result As Decimal = Minutes / workMinutes * 10000
+                ' Retains the whole number part
+                Dim wholeNumberPart As Integer = Int(result)
+                ' Converts result to string
+                Dim numberAsText As String = Format(result, "#,##0.00")
+                ' Gets the decimal part of numberAsText
+                Dim decimalPart As String = Right(numberAsText, 2)
+                ' Splits the decimal part
+                Dim decimalLeft As String = Left(decimalPart, 1)
+                Dim decimalRight As String = Right(decimalPart, 1)
+                ' If decimalRight is greater than CARRY_CRITERIA, adds 1 to decimalLeft
+                Dim carryOver As Integer = IIf(Int(decimalRight) > carryCriteria, Int(decimalLeft) + 1, Int(decimalLeft))
+                ' If carryOver is greater than CARRY_CRITERIA, adds 1 to wholeNumberPart
+                wholeNumberPart = IIf(carryOver > carryCriteria, wholeNumberPart + 1, wholeNumberPart)
+                ' Second iteration (converts wholeNumberPart to number with one decimal place)
+                result = wholeNumberPart / 10
+                ' Retains the whole number part
+                wholeNumberPart = Int(result)
+                ' Converts result to string
+                numberAsText = Format(result, "#,##0.0")
+                ' Gets the decimal part of numberAsText
+                decimalPart = Right(numberAsText, 1)
+                ' If decimalPart is Greater than CARRY_CRITERIA, adds one to wholeNumberPart
+                wholeNumberPart = IIf(Int(decimalPart) > carryCriteria, wholeNumberPart + 1, wholeNumberPart)
+                ' Returns wholeNumberPart as decimal number by dividing it by 1000
+                FractionsOfADay = wholeNumberPart / 1000
+            End If
+        Catch ex As Exception
+
+        End Try
+        Return FractionsOfADay
+    End Function
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Converts minutes to fractions of a day based on Civil Service Commission MC No. 14, s. 1999")>
+<Assembly: AssemblyDescription("Converts minutes to fractions of a day based on Civil Service Commission MC No. 14, s. 1999")>
+<Assembly: AssemblyCompany("Reynaldo Ibarra C. Tubianosa")>
+<Assembly: AssemblyProduct("FractionsOfADay.dll")>
+<Assembly: AssemblyCopyright("Reynaldo Ibarra C. Tubianosa Copyright ©  2022")>
+<Assembly: AssemblyTrademark("Reynaldo Ibarra C. Tubianosa")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("51a2d318-f8ea-4ddc-ba92-9b38893752be")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.2.0")>
+<Assembly: AssemblyFileVersion("1.0.2.0")>
+<Assembly: NeutralResourcesLanguage("en-US")>
